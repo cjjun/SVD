f2f3a48a4ced6f78ac580a61a462baa5e127b752
charles <cjjst123@qq.com>
2021-06-18 17:29:23 +0800

    update with function comple

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
-This is a ugly implementation of SVD with O(n^3) following two steps:
-(i)  Diagonizing symmetric matrix
-(ii) Calculating U,S,V
+# SVD implementation
+## Summary
+This is a naive implementation of SVD with $O(n^3)$ following two steps:
+-  Diagonizing symmetric matrix by $A^TA$ or $AA^T$
+- Calculating $U,S,V$
 
+## Diagonize symmetric matrix
+The basic ideas for diagonization of symmetric matrix are:
 
-(i)The basic ideas for diagonization of symmetric matrix are:
+- Use the iteration $x_{n+1} = ||Ax_{n}||$ , $x = x / ||x||$ to find x with respect to eigenvalue with largest absolute value. **THIS  WORKABLE AND STABLE ONLY FOR SYMMETRIC MATRIX**
 
-(1) Use the iteration x_{n+1} = ||Ax_{n}|| , x = x / ||x|| to find x with respect to eigenvalue with largest norm. THIS  WORKABLE AND STABLE ONLY FOR SYMMETRIC MATRIX
+- Every iteration amplifies the components in different eigenvalues subspaces with amplitude of norm of respecting eigenvalues. Using $Gram-smith$ normalization to eliminate projection on known eigenvector space. This has to be done onece every several iterations since even projection on previous eigenvectors with $10^{-16}$ precision error can be amplified only after several iterations and  take dominance.
 
-(2) Every iteration amplifies the components in different eigenvalues subspaces with amplitude of norm of respecting eigenvalues. Using Gram-smith normalization to eliminate projection on previous eigenvector space. This has to be done every iteration since even projection on previous eigenvectors with 1e-16 precision error can be amplified only after several iterations and  take dominance.
-
-(3) For zeros vectors, we use B = A + I to iterate. The previous eigenvalue - eigenvetor pairs are not changed with just increase 1 in each eigenvalues, while for remaining zero egienvalues would be changed to 1. We only do so to obtain orthogonal basis for null space.
+- For zeros vectors, we use $B = A + I$ instead to iterate. The previous eigenvalue - eigenvetor pairs are not changed with just increase 1 in each eigenvalues, while for remaining zero egienvalues would be changed to 1. We only do so to obtain orthogonal basis for null space.
   
-  
-(ii)The process for calculating SVD decomposition:
- 
-   (1) MV = A^T * A , MV = A * A^T. Seperately diagonize A MU and MV to obtain U,S,V.
+## SVD decomposition  
+Given target matrix $[A]_{n\times m} = USV^T$, suppose $m\ge n$, process for calculating SVD decomposition:
 
-   (2) The U and V might not be paired. That's to say qi * ui * vi^T might differ from the final decompoition with just a minus signal. So we use AV = US, AV and S to slightly adjust U. This process continues untill qi is no longer greater than 0. Since it makes no difference for qi = 0.
+   (1) $U\Sigma U^T = AA^T$, diagonize $AA^T$ to get $U$ and $\Sigma = SS^T$. Extract singular value from $\Sigma$ by square root and yield $S$.
+
+   (2) $SV^T = U^TA$, where $S$, $U$, $A$ are known. Solve the linear matrix equation and yield part of $V^T$. However, we only get part of vectors and still need to find the complimentary vectors that orthogonal to known vectors. This can be found with random vector generation and Gram-Smith.
+
+## Complexity
+   Step (1) takes $O(m^2n)$ to take matrix multiplication and $O(m^3)$ for diagonal decomposition. 
+
+   Step (2) takes $O(n^3)$. In total it takes $O(n^3)$.
--- SVD.py
+++ SVD.py
@@ -1,11 +1,27 @@
 import numpy as np 
 import math
 
+def comple(arr, valid):
+    assert np.shape(arr)[0] == np.shape(arr)[1], "Invalid matrix!"
+    iter = 5
+    n = arr.shape[0]
+    for i in range(valid, n):
+        x =  np.random.randn(n,1)
+        for j in range(5):
+            for k in range(i):
+                tmp = arr[:, k].reshape((-1, 1))
+                x -= tmp * tmp.T.dot(x)
+            x /= np.linalg.norm(x)
+
+        arr[:, i] = x[:, 0]
+    return arr            
+
+
 # Diagonize a real symmetric matrix(n*n), return eigenvalues vector(1*n) and orthogonal matrix(n*n)
 def Diag(a):                                        
     row,col = np.shape(a)
     U = np.zeros_like(a,dtype = 'float64')
-    S = np.zeros((1,row),dtype = 'float64')
+    S = np.zeros(row,dtype = 'float64')
     iter = 100
     iszero = 0
     for i in range(row):                        # Use the iteration x_{n+1} = ||Ax_{n}|| , x = x / ||x|| to find x with respect to eigenvalue with largest norm 
@@ -16,44 +32,62 @@ def Diag(a):
                 x -= np.reshape( U[:,k] * U[:,k].transpose().dot(x) , (row,1) )
 
             x = np.dot(a,x)
-            if np.linalg.norm(x) < 1e-12:       # The remaining eigenvalues are zeros, use B = A + I to undertake iteration
-                a = a + np.eye((row))
+            if np.linalg.norm(x) < 1e-12:       # The remaining eigenvalues are zeros, use complement operation
                 iszero = 1
-                x = np.random.randn(row,1)
-            x /= np.linalg.norm(x)
-        if not iszero:
-            S[0,i] = x.transpose().dot(a).dot(x)
-        else:
-            S[0,i] = 0
+                comple (U, i)
+                break
+            else:
+                x /= np.linalg.norm(x)
 
-        U[:,i] = x[:,0]
-    return S,U
+        if not iszero:
+            S[i] = x.transpose().dot(a).dot(x)
+            U[:,i] = x[:,0]
+        else:
+            break 
+    return S, U
 
 # The svd implementation with A(m*n)
-def SVD(A):                             
+def SVD(A):          
+    is_tranposed = False
+    if A.shape[0] > A.shape[1]:
+        is_tranposed = True
+        A = A.T   
+
     row,col = np.shape(A)
     MU = A.dot(A.transpose())           # Seperately calculate left/right eigenvector matrix
-    MV = A.transpose().dot(A)
 
     s,U = Diag(MU)
-    s,V = Diag(MV)
+    # print("----", U.shape)
 
-    S = np.zeros_like(A,dtype = 'float64')  # S is the sigma matrix(m*n) with singular values diagonally. 
-    for i in range( min(col,row) ):
-            S[i,i] = math.sqrt(s[0,i])
+    S = np.zeros_like(A, dtype = 'float64')  # S is the sigma matrix(m*n) with singular values diagonally. 
+    for i in range(row):
+        S[i,i] = math.sqrt(s[i])
 
-    av = A.dot(V)                           # The left and right eigenvectors might not be paired, with a '-' difference. AV = US, we use AV and S to adjust the minus signal in U
-   
-    for i in range( min(col,row) ):
-        if abs(S[i,i]) > 1e-12:             # If the singular values is zero, then no necessity to adjust U. But calculating U is necessary since singular value zeros can't help getting U(:,i)
-            U[:,i] = av[:,i] / S[i,i]
-    return U,S,V
+    MV = U.T.dot(A)
+    V = np.zeros( (col, col))
+    for i in range(col):
+        if i < row and S[i, i] > 1e-12:
+            V[:, i] = MV[i,:] / S[i, i]
+        else:
+            V = comple (V, i)
+            break
+    if not is_tranposed:
+        return U, S, V
+    else:
+        return V, S.T, U
 
 if __name__ == '__main__':
     a = np.array([[0,0.5,0.25],[0.5,0.25,0.125],[0.25,0.125,0.5*0.125]]) 
-    # a = a.transpose().dot(a)
     [u,s,v] = SVD(a)
     print(u)
     print(s)
     print(v)
-    # print(u.dot(s).dot(v.transpose()))
+    # for i in range(100):
+        # m = np.random.randint(1, 1001)
+        # n = np.random.randint(1, 11)
+        # a = np.random.rand(m, n) 
+        # print(a)
+        # print(i + 1)
+        # assert np.linalg.norm( u.T.dot(u) - np.eye (a.shape[0])) / np.size(a) < 1e-3, "U is not a valid normal matrix"
+        # assert np.linalg.norm( v.T.dot(v) - np.eye (a.shape[1]) ) / np.size(a) < 1e-3, "V is not a valid normal matrix"
+        # assert np.linalg.norm( u.dot(s).dot(v.T) - a ) / np.size(a) < 1e-3, "SVD decomposition is incorrect"
